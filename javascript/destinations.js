3991cca1bd8da13d3ccafb6d2c957a57e20cefd2
ermelinda150906-boop <ermelinda150906@gmail.com>
2025-11-27 17:26:43 +0100

    login

---
+++ javascript/destinations.js
@@ -0,0 +1,21 @@
+function ndryshoLondon(){
+  const imazhi = document.getElementById("foto");
+  imazhi.setAttribute("src", "https://image.urlaubspiraten.de/4x3/image/upload/v1603987364/mediavault_images/big_ben_london_city_trip_lk35rm.png");
+}
+  
+function ktheLondon(){
+  const imazhi = document.getElementById("foto");
+  imazhi.src = "./images/london.avif";
+}
+
+
+
+function ndryshoBarcelona(){
+const imazhi1 = document.getElementById("foto1");
+  imazhi1.setAttribute("src", "https://www.royalcaribbean.com/media-assets/pmc/content/dam/shore-x/barcelona-bcn/br05-barcelona-city-and-city-center-transfer/stock-photo-view-of-barcelona-from-the-park-at-sunrise-734963035.jpg?w=1920");
+}
+
+function ktheBarcelona(){
+  const imazhi1 = document.getElementById("foto1");
+  imazhi1.src = "./images/barcelona.avif";
+}
