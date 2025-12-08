adb1072575725c2677b97b8187c7ca7eee55d535
ermelinda150906-boop <ermelinda150906@gmail.com>
2025-12-08 16:12:06 +0100

    destinations.js

--- javascript/destinations.js
+++ javascript/destinations.js
@@ -1,3 +1,5 @@
+
+//londra
 function ndryshoLondon(){
   const imazhi = document.getElementById("foto");
   imazhi.setAttribute("src", "https://image.urlaubspiraten.de/4x3/image/upload/v1603987364/mediavault_images/big_ben_london_city_trip_lk35rm.png");
@@ -9,7 +11,7 @@ function ktheLondon(){
 }
 
 
-
+//Barcelona
 function ndryshoBarcelona(){
 const imazhi1 = document.getElementById("foto1");
   imazhi1.setAttribute("src", "https://www.royalcaribbean.com/media-assets/pmc/content/dam/shore-x/barcelona-bcn/br05-barcelona-city-and-city-center-transfer/stock-photo-view-of-barcelona-from-the-park-at-sunrise-734963035.jpg?w=1920");
@@ -19,3 +21,25 @@ function ktheBarcelona(){
   const imazhi1 = document.getElementById("foto1");
   imazhi1.src = "./images/barcelona.avif";
 }
+//qipro
+function ndryshoQipro(){
+const imazhi2 = document.getElementById("foto2");
+imazhi2.setAttribute("src", "https://www.thetimes.com/imageserver/image/%2F933b1bbb-f03b-4756-a213-69256c57b141.jpg?crop=3000%2C2000%2C0%2C0&resize=618");
+}
+
+function ktheQipro(){
+  const imazhi2 = document.getElementById("foto2");
+  imazhi2.src = "./images/Qipro.webp";
+}
+
+function ndryshoKroacia(){
+const imazhi3 = document.getElementById("foto3");
+  imazhi3.setAttribute("src", "https://images.zicasso.com/08d7b3cc1dc73e8abd9d692d27b9e53c.jpg");
+}
+
+function ktheKroacia(){
+  const imazhi3 = document.getElementById("foto3");
+  imazhi3.src = "./images/kroaciaa.jpg";
+}
+
+
