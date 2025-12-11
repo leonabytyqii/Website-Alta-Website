6deda8271ac5a4817ec14a9d681c10b46b5c973e
ermelinda150906-boop <ermelinda150906@gmail.com>
2025-12-11 15:49:21 +0100

    navbar ,menu per telefon

--- javascript/index.js
+++ javascript/index.js
@@ -77,3 +77,12 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+
+//menu
+const menu=document.getElementById("menu");
+const navLinks = document.getElementById("navLinks");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
